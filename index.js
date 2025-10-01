89d5d5a08960f0d9120550e7384afaeab74e320e
Jatin Jaiswal <jaiswaljatin47@gmail.com>
2025-10-02 02:00:29 +0530

    update 1.0.2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/latest-version', (req, res) => {
-  const latestVersion = process.env.LATEST_VERSION || "1.0.1+4";
+  const latestVersion = process.env.LATEST_VERSION || "1.0.2";
   const apkUrl = process.env.APK_URL || "https://drive.google.com/file/d/1dVQesTC2JhGcgYu1Yhz2k84DN6q5oHmL/view?usp=drivesdk";
   const updateNotes = process.env.UPDATE_NOTES || "Initial release.";
   const forceUpdate = process.env.FORCE_UPDATE === 'true';
